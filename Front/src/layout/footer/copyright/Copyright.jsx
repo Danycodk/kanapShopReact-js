b551930ca26343d19a4bbd527f8a2350cc510339
danycodk <danielkonate123@gmail.com>
2023-11-13 11:54:39 -0500

    header finish, pageHeader advance

---
+++ Front/src/layout/footer/copyright/Copyright.jsx
@@ -0,0 +1,16 @@
+import './Copyright.scss'
+
+function Copyright() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="copyryght">
+        <p>© Copyright 2023 - 2042 | PetCouch by DanycodK| All Rights Reserved | Powered by </p>
+    </div>
+      
+    </>
+  )
+}
+
+export default Copyright
